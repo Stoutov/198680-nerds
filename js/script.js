52946c288a0a746dd342988545073498554087ae
Stoutov <anton.tikhonovlll@yandex.ru>
2017-09-03 16:52:58 +0300

    JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
-
+var modalBtnOpen = document.querySelector('.contacts__btn');
+var modalBtnClose = document.querySelector('.modal-write__btn--close');
+var modalModalPopup = document.querySelector('.modal-write');
