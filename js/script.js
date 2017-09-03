3b8623dbd967a47906503ebae1f875e704896343
Stoutov <anton.tikhonovlll@yandex.ru>
2017-09-03 17:14:51 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 var modalBtnOpen = document.querySelector('.contacts__btn');
 var modalBtnClose = document.querySelector('.modal-write__btn--close');
 var modalModalPopup = document.querySelector('.modal-write');
+
+modalBtnOpen.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalModalPopup.classList.add("modal-write--show");
+});
+
+modalBtnClose.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  modalModalPopup.classList.remove("modal-write--show");
+});
